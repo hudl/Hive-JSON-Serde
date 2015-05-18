cdce8f06a71f000c32b4c3588460ddcc79f6ece9
Nathan DeMaria <nathandmaria@gmail.com>
2015-05-18 13:02:43 -0500

    Fixed tests that use non-lowercase struct names

--- json-serde/src/test/java/org/openx/data/jsonserde/NestedStructureTest.java
+++ json-serde/src/test/java/org/openx/data/jsonserde/NestedStructureTest.java
@@ -37,7 +37,7 @@ import org.openx.data.jsonserde.json.JSONObject;
 public class NestedStructureTest {
      static JsonSerDe instance;
 
-  //@Before
+  @Before
   public void setUp() throws Exception {
     initialize();
   }
@@ -62,10 +62,10 @@ public class NestedStructureTest {
      instance.initialize(conf, tbl);
   }
 
-  //@Test
+  @Test
   public void testDeSerialize() throws Exception {
     // Test that timestamp object can be deserialized
-    Writable w = new Text("{ \"kind\": \"youtube#videoListResponse\", \"etag\": \"\\\"79S54kzisD_9SOTfQLu_0TVQSpY/mYlS4-ghMGhc1wTFCwoQl3IYDZc\\\"\", \"pageInfo\": { \"totalResults\": 1, \"resultsPerPage\": 1 }, \"items\": [ { \"kind\": \"youtube#video\", \"etag\": \"\\\"79S54kzisD_9SOTfQLu_0TVQSpY/A4foLs-VO317Po_ulY6b5mSimZA\\\"\", \"id\": \"wHkPb68dxEw\", \"statistics\": { \"viewCount\": \"9211\", \"likeCount\": \"79\", \"dislikeCount\": \"11\", \"favoriteCount\": \"0\", \"commentCount\": \"29\" }, \"topicDetails\": { \"topicIds\": [ \"/m/02mjmr\" ], \"relevantTopicIds\": [ \"/m/0cnfvd\", \"/m/01jdpf\" ] } } ] }");
+    Writable w = new Text("{ \"kind\": \"youtube#videoListResponse\", \"etag\": \"\\\"79S54kzisD_9SOTfQLu_0TVQSpY/mYlS4-ghMGhc1wTFCwoQl3IYDZc\\\"\", \"pageinfo\": { \"totalresults\": 1, \"resultsPerPage\": 1 }, \"items\": [ { \"kind\": \"youtube#video\", \"etag\": \"\\\"79S54kzisD_9SOTfQLu_0TVQSpY/A4foLs-VO317Po_ulY6b5mSimZA\\\"\", \"id\": \"wHkPb68dxEw\", \"statistics\": { \"viewCount\": \"9211\", \"likeCount\": \"79\", \"dislikeCount\": \"11\", \"favoriteCount\": \"0\", \"commentCount\": \"29\" }, \"topicDetails\": { \"topicIds\": [ \"/m/02mjmr\" ], \"relevantTopicIds\": [ \"/m/0cnfvd\", \"/m/01jdpf\" ] } } ] }");
 
     JSONObject result = (JSONObject) instance.deserialize(w);
     
